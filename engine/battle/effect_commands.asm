082aba35ef9f56cd5c525a2a01a45df8056bedd3
Frank995 <pfrank995@gmail.com>
2025-09-18 15:16:44 +0200

    FIX pt2: Metal Powder can increase damage taken with boosted (Special) Defense

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -2604,11 +2604,13 @@ PlayerAttackDamage:
 	call ThickClubBoost
 
 .done
+	push hl
+	call DittoMetalPowder
+	pop hl
 	call TruncateHL_BC
 
 	ld a, [wBattleMonLevel]
 	ld e, a
-	call DittoMetalPowder
 
 	ld a, 1
 	and a
@@ -2855,11 +2857,14 @@ EnemyAttackDamage:
 	call ThickClubBoost
 
 .done
+	push hl
+	call DittoMetalPowder
+	pop hl
+
 	call TruncateHL_BC
 
 	ld a, [wEnemyMonLevel]
 	ld e, a
-	call DittoMetalPowder
 
 	ld a, 1
 	and a
